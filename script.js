c557349cfc9439c1018e628bdf3eae25976443cb
ankitautkarr <ankitautkarra@gmail.com>
2021-05-02 20:33:38 +0530

    JS Done

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-// function
-let toDoList = [];
+
 let toDoListTemp = [];
+let count = 0;
+
 function clickHandler() {
-  toDoList = [];
+  count++;
   let output = document.getElementById('input');
-  toDoList.push(output.value);
-  toDoListTemp.push(output.value);
-
-  toDoList.forEach((task) => {
-    let li = document.createElement('li'); // Create a <p> node
-    li.setAttribute('id', `li${toDoListTemp.indexOf(task)}`);
-
-    let icon = document.createElement('i');
-    icon.setAttribute('class', 'fas fa-trash delete');
-
+  if (output.value !== '') {
+    toDoListTemp.push(output.value);
+    let li = document.createElement('li');
+    li.setAttribute('id', `li${count}`);
+    li.innerHTML = `<i class="fas fa-trash delete" id = "del${count}"></i>`;
     document.getElementById('todolist').appendChild(li);
-    var t = document.createTextNode(task); // Create a text node
-    li.appendChild(t); // Append the text to <p>
-    li.appendChild(icon);
-    document.getElementById('todolist').appendChild(li); // Append <p> to <div> with id="myDIV"
+    var t = document.createTextNode(output.value);
+    li.appendChild(t);
+    document.getElementById('todolist').appendChild(li);
+    let del = document.getElementById(`del${count}`);
+    del.addEventListener('click', onClickDelete);
+    pendingTask(toDoListTemp);
+  }
+  output.value = '';
+}
 
-    let hr = document.createElement('hr');
-    document.getElementById('todolist').appendChild(hr);
-  });
+function pendingTask(array) {
   let pendingTaskEl = document.getElementById('pending-tasks');
-  pendingTaskEl.innerText = `You have ${toDoListTemp.length} pending tasks`;
+  pendingTaskEl.innerText = `You have ${array.length} pending tasks`;
+}
+
+function onClickClearAll() {
+  count = 0;
+  toDoListTemp = [];
+  toDoList = [];
+  pendingTask(toDoListTemp);
+  let parentEl = document.getElementById('todolist');
+  while (parentEl.firstChild) {
+    parentEl.removeChild(parentEl.firstChild);
+  }
+}
+
+function onClickDelete(event) {
+  let listElement = document.getElementById(event.target.parentElement.id);
+  listElement.remove();
+  let taskToPop = event.target.parentElement.innerText;
+  let index = toDoListTemp.indexOf(taskToPop);
+  if (index > -1) {
+    toDoListTemp.splice(index, 1);
+  }
+  pendingTask(toDoListTemp);
 }
